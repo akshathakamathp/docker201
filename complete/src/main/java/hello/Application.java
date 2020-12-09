a3a9c757036366636c013df2edc3669ffcd22867
M1037127 <M1037127@A2ML39943.mindtree.com>
2020-12-09 13:25:34 +0530

    Added get call for Redis

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,8 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +15,16 @@ public class Application {
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World";
+	}
+	
+	@GetMapping("/redis")
+	public String getRedisData() {
+		Jedis jedis = new Jedis("localhost");
+		System.out.println("Connection successful");
+		System.out.println("Getting response from the server: " + jedis.ping());
+		String pingReply = jedis.ping();
+		jedis.close();
+		return "Getting response from the server: " + pingReply;
 	}
 
 	public static void main(String[] args) {
