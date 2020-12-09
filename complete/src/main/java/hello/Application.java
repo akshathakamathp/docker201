3733e2bd93c463476cb984c4c42d27579610a22f
M1037127 <M1037127@A2ML39943.mindtree.com>
2020-12-09 17:34:44 +0530

    Added GET and PUT message

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,7 +3,9 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import redis.clients.jedis.Jedis;
@@ -18,11 +20,24 @@ public class Application {
 	}
 
 	@GetMapping("/redis")
-	public String getRedisData() {
-
+	public String getRedisData(@RequestParam String key) {
 		try (Jedis jedis = new Jedis("172.17.0.2", 6379, 5000)) {
 			System.out.println("Connection successful");
-			System.out.println("Getting response from the server: " + jedis.ping());
+			System.out.println("Getting response from the server: " + jedis.get(key));
+			String pingReply = jedis.get(key);
+			return "Getting response from the server: " + pingReply;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+		
+	}
+	
+	@PutMapping("/redis")
+	public String putRedisData(@RequestParam String key,@RequestParam String value) {
+		try (Jedis jedis = new Jedis("172.17.0.2", 6379, 5000)) {
+			System.out.println("Connection successful");
+			jedis.set(key, value);
 			String pingReply = jedis.ping();
 			return "Getting response from the server: " + pingReply;
 		} catch (Exception e) {
